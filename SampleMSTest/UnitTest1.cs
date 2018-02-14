131da4926609810c81aa338513407e53a174f7d0
Hicham Wahbi <hicham_wahbi@hotmail.com>
2018-02-14 05:32:48 -0600

    init

--- SampleMSTest/UnitTest1.cs
+++ SampleMSTest/UnitTest1.cs
@@ -7,8 +7,24 @@ namespace SampleMSTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SuccesTestMethod()
         {
+            int num = 20;
+            int denom = 4;
+
+            int quotient = num / denom;
+
+            Assert.AreEqual(quotient, 5);
+        }
+        [TestMethod]
+        public void FailureTestMethod()
+        {
+            int num = 20;
+            int denom = 4;
+
+            int quotient = num / denom;
+
+            Assert.AreEqual(quotient, 6);
         }
     }
 }
